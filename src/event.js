6212ae750b5f1fa34eb51f225c9e0558f6e8811b
talama <talama@protonmail.com>
2022-04-12 17:30:31 +0200

    Adds basic html page and implement Event class

---
+++ src/event.js
@@ -0,0 +1,19 @@
+/**
+ * Class Event
+ * An Event will notify its listeners every time the event is triggered
+ */
+class Event {
+  constructor() {
+    this.listeners = [];
+  }
+
+  addListner(listener) {
+    this.listeners.push(listener);
+  }
+
+  trigger(params) {
+    this.listeners.forEach((listener) => listener(params));
+  }
+}
+
+export default Event;
